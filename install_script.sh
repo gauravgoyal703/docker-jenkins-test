21728ba01a3852dca65085032f71ad76ce66d2e0
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-06 18:20:42 +1200

    Update install_script.sh
    Add docker for sonarqube

--- install_script.sh
+++ install_script.sh
@@ -17,6 +17,9 @@ chown -R 1000:1000 /var/jenkins_home/
 docker build -t jenkins:jcasc .
 docker run -p 8080:8080 -p 50000:50000 --env githubCodeRepoCredentials=testvalue --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password -v /var/jenkins_home:/var/jenkins_home -d --name jenkins --rm jenkins:jcasc
 
+# run sonar
+docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 -p 9092:9092 sonarqube:latest
+
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
