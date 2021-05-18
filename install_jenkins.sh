9834376b8ba7aa843db5a6b121ab023ed3b491b2
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-18 14:51:39 +1200

    Update install_jenkins.sh
    Copy env file

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,4 +4,6 @@ rm -r docker-jenkins-test
 
 git clone https://github.com/gauravgoyal703/docker-jenkins-test.git
 cd docker-jenkins-test
+mkdir config
+cp ../config/.env.dev config/.env.dev
 sh ./install_script.sh
