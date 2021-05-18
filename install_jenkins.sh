73af6a5a11c95f525ec354c5d987b4f4182e59df
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-18 16:03:22 +1200

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -5,5 +5,5 @@ rm -r docker-jenkins-test
 git clone https://github.com/gauravgoyal703/docker-jenkins-test.git
 cd docker-jenkins-test
 mkdir config
-cp ../config/envdev.txt config/.env.dev
+cp ../config/envdev.txt .env
 sh ./install_script.sh
